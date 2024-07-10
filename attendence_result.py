9647a155c1092cce891d8e8535284534f6c70bf2
Jaspreet singh <63514911+jassivx@users.noreply.github.com>
2024-07-10 11:40:46 +0530

    data creation is under process.

--- attendence_result.py
+++ attendence_result.py
@@ -1,44 +1,4 @@
-'''
 
-import pandas as pd
-
-# Reading the first CSV file
-try:
-    df_total_final = pd.read_csv('C://Users//AKASH VISHWAKARMA//Documents//attendence docs//attn_26Fjune.csv')
-    print("Dataframe 'total_final.csv' successfully created!")
-    print(df_total_final.columns)
-except FileNotFoundError:
-    print("Error: File 'total_final.csv' not found!")
-except pd.errors.ParserError:
-    print("Error: Couldn't parse the CSV file 'total_final.csv'!")
-
-# Reading the second CSV file
-try:
-    df_present_absent = pd.read_csv('C://Users//AKASH VISHWAKARMA//Documents//attendence docs//28 june attendence.csv')
-    print("Dataframe 'present+absent.csv' successfully created!")
-    print(df_present_absent.columns)
-except FileNotFoundError:
-    print("Error: File 'present+absent.csv' not found!")
-except pd.errors.ParserError:
-    print("Error: Couldn't parse the CSV file 'present+absent.csv'!")
-
-# Check if both dataframes were created successfully before merging
-if 'df_total_final' in locals() and 'df_present_absent' in locals():
-    # Merging the two dataframes on 'attn_id'
-    merged_df = pd.merge(df_total_final, df_present_absent, on='Attn_id', how='left')
-
-    # Display the merged dataframe
-    print("Merged DataFrame:")
-    print(merged_df)
-else:
-    print("Merging not possible. One or both DataFrames are not available.")
-
-dff = merged_df.to_csv('C://Users//AKASH VISHWAKARMA//Documents//attendence docs//attn_result.csv')
-
-
-
-
-'''
 
 import pandas as pd
 
@@ -54,7 +14,7 @@ except pd.errors.ParserError:
 
 # Reading the second CSV file
 try:
-    df_present_absent = pd.read_csv('C://Users//AKASH VISHWAKARMA//Documents//attendence docs/July 4.csv')
+    df_present_absent = pd.read_csv('C://Users//AKASH VISHWAKARMA//Documents//attendence docs/9 july 24.csv')
     print("Dataframe 'present+absent.csv' successfully created!")
     print(df_present_absent.columns)
 except FileNotFoundError:
@@ -83,5 +43,5 @@ else:
 
 
 print("below are the all feature columns",merged_df.columns)
-merged_df = merged_df.drop(columns = ['designation',' Division/Units_x',' Office Locations_x','In Time_x','Out Time_x','Duration_x','Name'])
-merged_df.to_csv('C://Users//AKASH VISHWAKARMA//Documents//attendence docs//attn July 4.csv', index=False)
+merged_df = merged_df.drop(columns = [' Employee Designation',' Division/Units_x',' Office Locations_y','In Time_x','Out Time_x','Duration_x','Name'])
+merged_df.to_csv('C://Users//AKASH VISHWAKARMA//Documents//attendence docs//attn July 9(1).csv', index=False)
