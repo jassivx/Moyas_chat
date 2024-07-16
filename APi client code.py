5af5d51f8b687c5c60f8c9f25dc8aa47aa832801
Jaspreet singh <63514911+jassivx@users.noreply.github.com>
2024-07-16 17:18:45 +0530

    data creation is under process.

---
+++ APi client code.py
@@ -0,0 +1,5 @@
+
+from datagovindia import DataGovIndia
+
+YOUR_API_KEY =  “579b464db66ec23bd********************7209ff7b23ac571b”
+datagovin = DataGovIndia(YOUR_API_KEY)
--- APi server code.py
+++ APi server code.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, jsonify, request
 import pandas as pd
 
@@ -6,32 +7,35 @@ app = Flask(__name__)
 # Function to read the CSV file and return it as a list of dictionaries
 def csv_data():
     try:
-        data = pd.read_csv('C://Users//AKASH VISHWAKARMA//Documents//attendence docs//attn July9.csv')
-        return data.to_dict(orient='records')
+        data = pd.read_csv('C://Users//AKASH VISHWAKARMA//Documents//attendence docs//July 111.csv')
+        return data.to_dict(orient='records'), None
     except FileNotFoundError:
-        print("File not found")
-        return None
+        error_message = "File not found"
+        return None, error_message
     except pd.errors.EmptyDataError:
-        print("There is no data in the file")
-        return None
-
+        error_message = "There is no data in the file"
+        return None, error_message
     except Exception as e:
-        print("there is some error in reading the file, that error is{e} ")
-        return None
+        error_message = f"There is some error in reading the file, that error is {e}"
+        return None, error_message
 
 # Endpoint to get all users
 @app.route('/users', methods=['GET'])
 def get_users():
-    data = csv_data()
-    return jsonify(data) #This informs the client application(a web browser or another API)-
-                         # -that the data being sent is in JSON format.
+    print("Endpoint /users called")
+    data, error = csv_data()
+    if error:
+        return jsonify({'error is-': error}), 500
+    return jsonify(data)  # This informs the client application (a web browser or another API) that the data being sent is in JSON format.
 
-#Creating an Endpoint to get a specific user by ID
+# Creating an Endpoint to get a specific user by ID
 @app.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    data = csv_data()
-    user = None
+    data, error = csv_data()
+    if error:
+        return jsonify({'error': error}), 500
 
+    user = None
     for item in data:
         if int(item['id']) == user_id:
             user = item
@@ -45,3 +49,4 @@ def get_user(user_id):
 # Run the Flask app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
